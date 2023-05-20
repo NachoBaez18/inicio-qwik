9bae80b6ee04ba241841fddf94944a2318045df6
Nacho <baeznacho0@gmail.com>
2023-05-20 11:14:57 -0400

    terminamos seccion de mostar pokemon

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,34 +1,35 @@
 import { $, component$, useSignal } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
+import { PokemonImage } from '~/components/pokemons/pokemon-image';
 
 
 export default component$(() => {
 
   const pokemonId = useSignal(1); //
+  const imageBack = useSignal(false);
+  const isVisibleaqui = useSignal(true);
   const changePokemonId = $((value:number) =>{
     if ((pokemonId.value + value)<= 0) return;
 
     pokemonId.value += value;
+    isVisibleaqui.value = true;
+    
   })
-
+  
   return (
     <>
       <span class="text-2xl">Buscador Simple</span>
 
       <span class="text-9xl">{pokemonId}</span>
 
-      <img 
-      src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId.value}.png`}
-      alt="Pokemon Sprite" 
-      style={{with:'200px'}}
-      height={200}
-      width={200}
-      />
+      {/*  */}
+      <PokemonImage id = {pokemonId.value} backImage = {imageBack.value} isVisible= {isVisibleaqui.value}/>
 
       <div class="mt-2">
         <button onClick$={() => changePokemonId(-1)} class = "btn btn-primary mr-2">Anterior</button>
-        <button onClick$={() => changePokemonId(+1)} class = "btn btn-primary">Siguiente</button>
-
+        <button onClick$={() => changePokemonId(+1)} class = "btn btn-primary mr-2">Siguiente</button>
+        <button onClick$={() => imageBack.value = !imageBack.value} class = "btn btn-primary mr-2">Voltear</button>
+        <button onClick$={() => isVisibleaqui.value = !isVisibleaqui.value} class = "btn btn-primary mr-2">Revelar</button>
       </div>
 
     </>
